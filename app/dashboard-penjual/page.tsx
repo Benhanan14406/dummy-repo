c7ccc506b1eab4cd50dfc1ba17fe1977fa8f247f
alyhaidar11 <m.alyhaidarulloh@gmail.com>
2026-05-03 22:54:33 +0700

    navbar

---
+++ app/dashboard-penjual/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Navbar from "@/component/navbar";
+
+export default function Dashboard_penjual(){
+
+    return (
+        <div>
+            <Navbar/>
+
+          
+
+        </div>
+    )
+}
