6b1df75fe9bbeaee2271de1a1ca8a681df53130b
DionWsdm <dionwisdom29@gmail.com>
2026-05-05 15:53:36 +0700

    feat: add shadcn and setup css

---
+++ utils/font.ts
@@ -0,0 +1,7 @@
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+    variable: "--font-poppins",
+    subsets: ["latin"]
+});
